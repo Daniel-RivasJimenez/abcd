6a1ebce576093029411c5ab3493e09ce60f4bba9
Daniel-RivasJimenez <leinadsavir30@gmail.com>
2024-03-12 16:43:22 +0100

    final

---
+++ apuntes/apuntes.py
@@ -0,0 +1,19 @@
+Esto hacerlo en el terminal
+
+git add .
+git commit -m "final"
+git push
+
+
+crear Environment
+python .m venv Daniel-env
+
+
+seleccionar la carpeta
+Daniel-env\Scripts\Activate.ps1
+
+para eliminar este error:
+Set-ExecutionPolicy Bypass -Scope Process
+
+instalar librerias
+pip install -r .\CODE\requirements.txt         'lugar donde esta la carpeta/folder de requirements'   
